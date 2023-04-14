d7b841787c30cc0483c8ea59ebe16ca50124ef91
alejandraparada <carla.parada.aguilera@gmail.com>
2023-04-14 14:43:56 +0200

    Energetico wab

---
+++ Energetico/widgets/CalificacionEdificios/Widget.js
@@ -0,0 +1,332 @@
+define([
+  'dojo/_base/declare', 
+  'dojo/dom',
+  "dojo/on",
+  "dojo/_base/lang",
+
+  "esri/map",
+  "esri/layers/FeatureLayer",
+  "esri/dijit/PopupTemplate",
+
+  'jimu/BaseWidget'
+],
+  function(
+    declare, 
+    dom,
+    on,
+    lang,
+
+    map,
+    FeatureLayer,
+    PopupTemplate,
+
+    BaseWidget
+    ) {
+    return declare([BaseWidget], {
+
+      baseClass: 'jimu-widget-customwidget',
+      
+      postCreate: function(){
+        this.currentLayer = this.map.getLayer(this.map.itemInfo.itemData.operationalLayers[1].id)
+        console.log("Map PTC",this.map);
+        console.log("Layer PTC",this.currentLayer);
+        
+      },
+      startup: function(){
+        console.log("Layer STP",this.currentLayer);
+      },
+      
+      filtrarInstaladores: function() {
+        // Definir expresiones Emisiones
+        var or = ` OR `;
+        var and = ` AND `;
+        var primaria = `CALIFICACION_E_PRIMARIA = `;
+        var emisiones = `CALIFICACION_EMISIONES_CO2 = `;
+        var calefaccion = `CALIFICACION_DEMANDA_CALEF_ = `;
+        var refrigeracion = `CALIFICACION_DEMANDA_REFRIG_ = `;
+        var aE = ``;
+        var bE = ``;
+        var cE = ``;
+        var dE = ``;
+        var eE = ``;
+        var fE = ``;
+        var gE = ``;
+
+        var codDistrito= `DISTRITO = `;
+        var expression = ``;
+
+        // Obtener distrito de texto
+        var distrito = this.selectDistrito.value;
+
+        // Obtener valor del select
+        var calificacion = this.selectCalificacion.value;
+        console.log(calificacion)
+
+        // Switch activados
+        var a = dom.byId("inp-a").checked;
+        var b = dom.byId("inp-b").checked;
+        var c = dom.byId("inp-c").checked;
+        var d = dom.byId("inp-d").checked;
+        var e = dom.byId("inp-e").checked;
+        var f = dom.byId("inp-f").checked;
+        var g = dom.byId("inp-g").checked;
+
+        if(calificacion=="CALIFICACION_E_PRIMARIA"){
+          var aE = `${primaria} 'A'`;
+          var bE = `${primaria} 'B'`;
+          var cE = `${primaria} 'C'`;
+          var dE = `${primaria} 'D'`;
+          var eE = `${primaria} 'E'`;
+          var fE = `${primaria} 'F'`;
+          var gE = `${primaria} 'G'`;
+        }else if(calificacion=="CALIFICACION_EMISIONES_CO2"){
+          var aE = `${emisiones} 'A'`;
+          var bE = `${emisiones} 'B'`;
+          var cE = `${emisiones} 'C'`;
+          var dE = `${emisiones} 'D'`;
+          var eE = `${emisiones} 'E'`;
+          var fE = `${emisiones} 'F'`;
+          var gE = `${emisiones} 'G'`;
+        }else if(calificacion=="CALIFICACION_DEMANDA_CALEF_"){
+          var aE = `${calefaccion} 'A'`;
+          var bE = `${calefaccion} 'B'`;
+          var cE = `${calefaccion} 'C'`;
+          var dE = `${calefaccion} 'D'`;
+          var eE = `${calefaccion} 'E'`;
+          var fE = `${calefaccion} 'F'`;
+          var gE = `${calefaccion} 'G'`;
+        }else if(calificacion=="CALIFICACION_DEMANDA_REFRIG_"){
+          var aE = `${refrigeracion} 'A'`;
+          var bE = `${refrigeracion} 'B'`;
+          var cE = `${refrigeracion} 'C'`;
+          var dE = `${refrigeracion} 'D'`;
+          var eE = `${refrigeracion} 'E'`;
+          var fE = `${refrigeracion} 'F'`;
+          var gE = `${refrigeracion} 'G'`;
+        }
+
+
+        if (distrito== 0){
+          if(a == true){
+            expression = expression + aE
+            
+            console.log("a",expression);
+            if(b == true){
+              expression = expression + or + bE;
+            }
+            if(c == true){
+              expression = expression + or + cE;
+            }
+            if(d == true){
+              expression = expression + or + dE;
+            }
+            if(e == true){
+              expression = expression + or + eE;
+            }
+            if(f == true){
+              expression = expression + or + fE;
+            }
+            if(g == true){
+              expression = expression + or + gE;
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer);
+          }else if(b == true){
+              expression = expression + bE;
+              if(c == true){
+                expression = expression + or + cE;
+              }
+              if(d == true){
+                expression = expression + or + dE;
+              }
+              if(e == true){
+                expression = expression + or + eE;
+              }
+              if(f == true){
+                expression = expression + or + fE;
+              }
+              if(g == true){
+                expression = expression + or + gE;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else if(c == true){
+              expression = expression + cE;
+              if(d == true){
+                expression = expression + or + dE;
+              }
+              if(e == true){
+                expression = expression + or + eE;
+              }
+              if(f == true){
+                expression = expression + or + fE;
+              }
+              if(g == true){
+                expression = expression + or + gE;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else if(d == true){
+              expression = expression + dE;
+              if(e == true){
+                expression = expression + or + eE;
+              }
+              if(f == true){
+                expression = expression + or + fE;
+              }
+              if(g == true){
+                expression = expression + or + gE;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else if(e == true){
+              expression = expression + eE;
+              if(f == true){
+                expression = expression + or + fE;
+              }
+              if(g == true){
+                expression = expression + or + gE;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else if(f == true){
+              expression = expression + fE;
+              if(g == true){
+                expression = expression + or + gE;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else if(g == true){
+              expression = expression + gE
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else{
+              alert("Elige al menos un producto")
+            }
+        }else if(a == true){
+          expression = "("+ expression + aE + ")" + and + codDistrito + distrito; 
+          console.log("a",expression);
+          if(b == true){
+            expression = "("+ expression + or + bE + ")" + and + codDistrito + distrito; 
+          }
+          if(c == true){
+            expression = "("+ expression + or + cE + ")" + and + codDistrito + distrito; 
+          }
+          if(d == true){
+            expression = "("+ expression + or + dE + ")" + and + codDistrito + distrito; 
+          }
+          if(e == true){
+            expression = "("+ expression + or + eE + ")" + and + codDistrito + distrito; 
+          }
+          if(f == true){
+            expression = "("+ expression + or + fE + ")" + and + codDistrito + distrito; 
+          }
+          if(g == true){
+            expression = "("+ expression + or + gE + ")" + and + codDistrito + distrito; 
+          }
+          console.log("fin",expression);
+          this.currentLayer.setDefinitionExpression(expression);
+          this.map.addLayer(this.currentLayer);
+        }else if(b == true){
+            expression = "("+ expression + bE + ")" + and + codDistrito + distrito; 
+            if(c == true){
+              expression = "("+ expression + or + cE + ")" + and + codDistrito + distrito; 
+            }
+            if(d == true){
+              expression = "("+ expression + or + dE + ")" + and + codDistrito + distrito; 
+            }
+            if(e == true){
+              expression = "("+ expression + or + eE + ")" + and + codDistrito + distrito; 
+            }
+            if(f == true){
+              expression = "("+ expression + or + fE + ")" + and + codDistrito + distrito; 
+            }
+            if(g == true){
+              expression = "("+ expression + or + gE + ")" + and + codDistrito + distrito; 
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else if(c == true){
+            expression = "("+ expression + cE + ")" + and + codDistrito + distrito; 
+            if(d == true){
+              expression = "("+ expression + or + dE + ")" + and + codDistrito + distrito; 
+            }
+            if(e == true){
+              expression = "("+ expression + or + eE + ")" + and + codDistrito + distrito; 
+            }
+            if(f == true){
+              expression = "("+ expression + or + fE + ")" + and + codDistrito + distrito; 
+            }
+            if(g == true){
+              expression = "("+ expression + or + gE + ")" + and + codDistrito + distrito; 
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else if(d == true){
+            expression = "("+ expression + dE + ")" + and + codDistrito + distrito; 
+            if(e == true){
+              expression = "("+ expression + or + eE + ")" + and + codDistrito + distrito; 
+            }
+            if(f == true){
+              expression = "("+ expression + or + fE + ")" + and + codDistrito + distrito; 
+            }
+            if(g == true){
+              expression = "("+ expression + or + gE + ")" + and + codDistrito + distrito; 
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else if(e == true){
+            expression = "("+ expression + eE + ")" + and + codDistrito + distrito; 
+            if(f == true){
+              expression = "("+ expression + or + fE + ")" + and + codDistrito + distrito; 
+            }
+            if(g == true){
+              expression = "("+ expression + or + gE + ")" + and + codDistrito + distrito; 
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else if(f == true){
+            expression = "("+ expression + fE + ")" + and + codDistrito + distrito; 
+            if(g == true){
+              expression = "("+ expression + or + gE + ")" + and + codDistrito + distrito; 
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else if(g == true){
+            expression = "("+ expression + gE + ")" + and + codDistrito + distrito; 
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else{
+            alert("Elige al menos un producto")
+          }
+
+        
+      },
+
+      refresh: function (){
+        this.selectDistrito.value = "0";
+        dom.byId("inp-a").checked = true;
+        dom.byId("inp-b").checked = true;
+        dom.byId("inp-c").checked = true;
+        dom.byId("inp-d").checked = true;
+        dom.byId("inp-e").checked = true;
+        dom.byId("inp-f").checked = true;
+        dom.byId("inp-g").checked = true;
+        this.currentLayer.setDefinitionExpression(this.currentLayer.defaultDefinitionExpression);
+      }
+
+    });
+  });
---
+++ Energetico/widgets/CombustibleEdificios/Widget.js
@@ -0,0 +1,186 @@
+define([
+  'dojo/_base/declare', 
+  'dojo/dom',
+  "dojo/on",
+  "dojo/_base/lang",
+
+  "esri/map",
+  "esri/layers/FeatureLayer",
+  "esri/dijit/PopupTemplate",
+
+  'jimu/BaseWidget'
+],
+  function(
+    declare, 
+    dom,
+    on,
+    lang,
+
+    map,
+    FeatureLayer,
+    PopupTemplate,
+
+    BaseWidget
+    ) {
+    return declare([BaseWidget], {
+
+      baseClass: 'jimu-widget-customwidget',
+      
+      postCreate: function(){
+        this.currentLayer = this.map.getLayer(this.map.itemInfo.itemData.operationalLayers[1].id)
+        console.log("Map PTC",this.map);
+        console.log("Layer PTC",this.currentLayer);
+        
+      },
+      startup: function(){
+        console.log("Layer STP",this.currentLayer);
+      },
+      
+      filtrarInstaladores: function() {
+        // Definir expresiones Emisiones
+        var or = ` OR `;
+        var and = ` AND `;
+        var ensenanza = `TIPO_DE_CENTRO = '0'`;
+        var deportivo = `TIPO_DE_CENTRO = '1`;
+        var sanitario = `TIPO_DE_CENTRO = '2'`;
+        var biblioteca = `TIPO_DE_CENTRO = '3'`;
+        var administrativo = `TIPO_DE_CENTRO = '4'`;
+        var autobuses = `TIPO_DE_CENTRO = '5'`;
+        var ferrocarril = `TIPO_DE_CENTRO = '6'`;
+        var mercado = `TIPO_DE_CENTRO = '7'`;
+        var museo = `TIPO_DE_CENTRO = '8'`;
+        var policia = `TIPO_DE_CENTRO = '9'`;
+        var residencia = `TIPO_DE_CENTRO = '10'`;
+
+
+        var aC = `ENERGIA_UTILIZADA_CALEFACCION = 'GASÓLEO / FUEL'`;
+        var bC = `ENERGIA_UTILIZADA_CALEFACCION = 'GAS NATURAL'`;
+        var cC = `ENERGIA_UTILIZADA_CALEFACCION = 'BIOMASA'`;
+
+        var codDistrito= `DISTRITO = `;
+        var tExpression = ``;
+        var expression = ``;
+
+        // Obtener distrito de texto
+        var distrito = this.selectDistrito.value;
+
+        // Obtener valor del select
+        var tipoCentro = this.selectTipo.value;
+        console.log(tipoCentro)
+
+        // Switch activados
+        var a = dom.byId("inp-a").checked;
+        var b = dom.byId("inp-b").checked;
+        var c = dom.byId("inp-c").checked;
+
+        if(tipoCentro=="0"){
+          tExpression = tExpression + ensenanza;
+        }else if(tipoCentro=="1"){
+          expression = expression + deportivo;
+        }else if(tipoCentro=="2"){
+          tExpression = tExpression + sanitario;
+        }else if(tipoCentro=="3"){
+          tExpression = tExpression + biblioteca;
+        }else if(tipoCentro=="4"){
+          tExpression = tExpression + administrativo;
+        }else if(tipoCentro=="5"){
+          tExpression = tExpression + autobuses;
+        }else if(tipoCentro=="6"){
+          tExpression = tExpression + ferrocarril;
+        }else if(tipoCentro=="7"){
+          tExpression = tExpression + mercado;
+        }else if(tipoCentro=="8"){
+          tExpression = tExpression + museo;
+        }else if(tipoCentro=="9"){
+          tExpression = tExpression + policia;
+        }else if(tipoCentro=="10"){
+          tExpression = tExpression + residencia;
+        }else if(tipoCentro=="11"){
+          tExpression = ``;
+        }
+
+        if (distrito== 0){
+          if(a == true){
+            expression = expression + aC;
+            console.log("a",expression);
+            if(b == true){
+              expression = expression + or + bC;
+            }
+            if(c == true){
+              expression = expression + or + cC;
+            }
+            if (tExpression !=``){
+              expression = "(" + expression + ")"+ and + tExpression;
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer);
+          }else if(b == true){
+              expression = expression + bC;
+              if(c == true){
+                expression = expression + or + cC;
+              }
+              if (tExpression !=``){
+                expression = "(" + expression + ")"+ and + tExpression;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }else if(c == true){
+              expression = expression + cC;
+              if (tExpression !=``){
+                expression = "(" + expression + ")"+ and + tExpression;
+              }
+              console.log("fin",expression);
+              this.currentLayer.setDefinitionExpression(expression);
+              this.map.addLayer(this.currentLayer)
+            }
+        }else if(a == true){
+          expression = "("+ expression + aC + ")" + and + codDistrito + distrito; 
+          console.log("a",expression);
+          if(b == true){
+            expression = "("+ expression + or + bC + ")" + and + codDistrito + distrito; 
+          }
+          if(c == true){
+            expression = "("+ expression + or + cC + ")" + and + codDistrito + distrito; 
+          }
+          if (tExpression !=``){
+            expression = "(" + expression + ")"+ and + tExpression;
+          }
+          console.log("fin",expression);
+          this.currentLayer.setDefinitionExpression(expression);
+          this.map.addLayer(this.currentLayer);
+        }else if(b == true){
+            expression = "("+ expression + bC + ")" + and + codDistrito + distrito; 
+            if(c == true){
+              expression = "("+ expression + or + cC + ")" + and + codDistrito + distrito; 
+            }
+            if (tExpression !=``){
+              expression = "(" + expression + ")"+ and + tExpression;
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }else if(c == true){
+            expression = "("+ expression + cC + ")" + and + codDistrito + distrito; 
+            if (tExpression !=``){
+              expression = "(" + expression + ")"+ and + tExpression;
+            }
+            console.log("fin",expression);
+            this.currentLayer.setDefinitionExpression(expression);
+            this.map.addLayer(this.currentLayer)
+          }
+
+      },
+
+      refresh: function (){
+        this.selectDistrito.value = "0";
+        this.selectTipo.value = "11"
+        dom.byId("inp-a").checked = true;
+        dom.byId("inp-b").checked = true;
+        dom.byId("inp-c").checked = true;
+        this.currentLayer.setDefinitionExpression(this.currentLayer.defaultDefinitionExpression);
+      }
+
+    });
+  });
